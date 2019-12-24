25889b805592a5283af2bd2dd2887458672ef481
jinhaixu <engkim0826@hotmail.com>
2019-12-24 16:02:07 +0900

    Updates

---
+++ precache-manifest.0259d02d333a70e97e558192f218d5ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98f70b1eafb4b7296e4e205caafdc997",
+    "url": "/React-Todo-App/index.html"
+  },
+  {
+    "revision": "007a262c3a68ba920082",
+    "url": "/React-Todo-App/static/css/main.372de89b.chunk.css"
+  },
+  {
+    "revision": "e324a88aad8ea6e315a1",
+    "url": "/React-Todo-App/static/js/2.b24ee4ae.chunk.js"
+  },
+  {
+    "revision": "007a262c3a68ba920082",
+    "url": "/React-Todo-App/static/js/main.9516bc7b.chunk.js"
+  },
+  {
+    "revision": "3b2db785ef38cb7deae3",
+    "url": "/React-Todo-App/static/js/runtime-main.17c7d03d.js"
+  }
+]);
